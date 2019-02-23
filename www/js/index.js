f3605c9d72771e6e38ea981ea5ec0fcbb963bf64
Kyle Baran <kbaran@bitscoop.com>
2019-02-22 18:39:45 -0800

    Updated to use inAppBrowser to load page in Cordova WebView, allowing use of cookies.
    Need to figure out how to reload homepage once user returns to app.

--- www/js/index.js
+++ www/js/index.js
@@ -1,31 +1,43 @@
-var app = {
+let app = {
     // Application Constructor
     initialize: function() {
-    this.bindEvents();
+        this.bindEvents();
     },
+
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+	    document.addEventListener('deviceready', this.onDeviceReady, false);
+	    document.addEventListener('pause', this.onPause, false);
+	    document.addEventListener('resume', this.onResume, false);
     },
+
+	onPause: function() {
+        console.log('PAUSE FIRED');
+	},
+
+	onResume: function() {
+    	console.log('RESUME FIRED');
+	},
+
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
+        window.open = cordova.InAppBrowser.open;
+
         // Here, we redirect to the web site.
-        var targetUrl = "https://app.lifescope.io";
-        var bkpLink = document.getElementById("bkpLink");
-        bkpLink.setAttribute("href", targetUrl);
-        bkpLink.text = targetUrl;
-        window.location.replace(targetUrl);
-},
+        let targetUrl = "https://app.lifescope.io?client=app";
+	    cordova.InAppBrowser.open(targetUrl, '_self', 'location=no');
+    },
+
     // Note: This code is taken from the Cordova CLI template.
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        let parentElement = document.getElementById(id);
+        let listeningElement = parentElement.querySelector('.listening');
+        let receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+	    console.log('Received Event: ' + id);
     }
 };
 
